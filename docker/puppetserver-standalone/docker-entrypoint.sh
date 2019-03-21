501f4565fa3846e2ceadc07ec5eaf4a383dfe25c
Morgan Rhodes <morgan@puppet.com>
2019-03-21 13:41:38 -0700

    (maint) consul agent spike

--- docker/puppetserver-standalone/docker-entrypoint.sh
+++ docker/puppetserver-standalone/docker-entrypoint.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
+on_exit() {
+  # Propagate up the exit code of our container application
+  status=$?
+  # Perform cleanup/whatever on exit
+  echo "Shutting down!"
+  # Don't trigger this again when we exit below
+  trap '' EXIT TERM
+  if [ "$CONSUL_ENABLED" = "true" ]; then
+    pkill consul
+  fi
+  exit $status
+
+}
+
 for f in /docker-entrypoint.d/*.sh; do
-    echo "Running $f"
-    chmod +x "$f"
-    "$f"
+  echo "Running $f"
+  chmod +x "$f"
+  "$f"
 done
 
-exec /opt/puppetlabs/bin/puppetserver "$@"
+trap on_exit INT TERM EXIT
+
+gosu root /opt/puppetlabs/bin/puppetserver "$@"
